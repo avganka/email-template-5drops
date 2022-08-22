1e428153546c13c0509106f7c83faf7665b2766e
Alex Kalmuk <karambasterg@gmail.com>
2022-08-22 14:11:34 +0300

    add files

---
+++ src/options.js
@@ -0,0 +1,136 @@
+const OPTIONS = {
+  categories: [
+    {
+      title: 'Лабораторная посуда "5drops.ru"',
+      imageURL:
+        "https://5drops.ru/upload/resize_cache/iblock/b35/450_450_140cd750bba9870f18aada2478b24840a/05mx74uwe1eos9jpqxcy6vm492hvaxss.jpg",
+      link: "https://5drops.ru/catalog/laboratornaya_posuda/borosilikatnaya_laboratornaya_posuda_5drops_ru/",
+    },
+    {
+      title: "Вакуумные пробирки",
+      imageURL:
+        "https://5drops.ru/upload/medialibrary/ca5/ca5e775e6a79b4e9b3de71983de560c6.jpg",
+      link: "https://5drops.ru/catalog/raskhodnye_materialy_dlya_laboratorii_i_analizov/probirki_dlya_vzyatiya_krovi/",
+    },
+    {
+      title: "Лабораторное оборудования",
+      imageURL:
+        "https://5drops.ru/upload/medialibrary/44a/44a9384200c0fd935a8404478c0d221a.jpg",
+      link: "https://5drops.ru/catalog/laboratornoe_oborudovanie/",
+    },
+    {
+      title: "Лабораторная посуда",
+      imageURL:
+        "https://5drops.ru/upload/medialibrary/282/2824bd5a32218ea9361a8250911c7814.jpg",
+      link: "https://5drops.ru/catalog/laboratornaya_posuda/",
+    },
+    {
+      title: "Расходные материалы для лабораторий",
+      imageURL:
+        "https://5drops.ru/upload/resize_cache/iblock/07e/450_450_140cd750bba9870f18aada2478b24840a/swq3sk65izahqb9ptj966oh68p51lt0x.jpg",
+      link: "https://5drops.ru/catalog/raskhodnye_materialy_dlya_laboratorii_i_analizov/",
+    },
+    {
+      title: "Стеклянные емкости",
+      imageURL:
+        "https://5drops.ru/upload/resize_cache/iblock/411/350_350_1/tfupxju97mfzoaqg0g9ywjbklwf02o66.jpg",
+      link: "https://5drops.ru/catalog/steklyannye_emkosti/",
+    },
+  ],
+  goods: [
+    {
+      title: 'Спиртовка СЛ-1, "V=100 мл"',
+      imageURL:
+        "https://5drops.ru/upload/resize_cache/iblock/9f5/450_450_140cd750bba9870f18aada2478b24840a/3xgu9dzcwq4oejmszsr7d9hl9sqz0g8x.jpg",
+      link: "https://5drops.ru",
+      retailPrice: "4500",
+      wholesalePrice: "2500",
+    },
+    {
+      title: 'Спиртовка СЛ-1, "V=100 мл"',
+      imageURL:
+        "https://5drops.ru/upload/resize_cache/iblock/9f5/450_450_140cd750bba9870f18aada2478b24840a/3xgu9dzcwq4oejmszsr7d9hl9sqz0g8x.jpg",
+      link: "https://5drops.ru",
+      retailPrice: "4500",
+      wholesalePrice: "2500",
+    },
+  ],
+  menu: [
+    {
+      title: "Каталог",
+      link: "https://5drops.ru/catalog/",
+    },
+    {
+      title: "Прайс-лист",
+      link: "https://5drops.ru/price.php",
+    },
+    {
+      title: "Опт",
+      link: "https://5drops.ru/sale/optovye_tseny/",
+    },
+    {
+      title: "Контакты",
+      link: "https://5drops.ru/contacts/",
+    },
+  ],
+  bannerDark: {
+    textColor: "white",
+    title: "Лабораторная посуда",
+    subtitle:
+      "Собственное производство боросиликатной лабораторной посуды СТМ 5drops",
+    link: "https://5drops.ru/catalog/laboratornaya_posuda/borosilikatnaya_laboratornaya_posuda_5drops_ru/",
+    imageURL: "https://affypharma.com/wp-content/uploads/2020/07/1563.jpg",
+  },
+  bannerUni: {
+    imageURL:
+      "https://5drops.ru/upload/iblock/61b/61ba8de99c11618a3e5689822ec8c44a.jpg",
+  },
+  bannerLight: {
+    textColor: "black",
+    title: "Лабораторное оборудование",
+    subtitle:
+      "Аналитическое и общелабораторное оборудование собственной торговой марки. В наличии на складе",
+    link: "https://5drops.ru/catalog/laboratornoe_oborudovanie/laboratornoe_oborudovanie_5drops_ru/",
+    imageURL:
+      "https://5drops.ru/upload/iblock/52c/52cf3f8af734b6864a4235956538c27e.jpg",
+  },
+  contacts: {
+    site: "https://5drops.ru",
+    email: "promo@5drops.ru",
+    phone: "+7 (499) 348-82-13",
+  },
+  socials: [
+    {
+      title: "Telegram",
+      imageURL:
+        "https://cdn.tools.unlayer.com/social/icons/rounded/telegram.png",
+      link: "https://t.me/chat_5drops",
+    },
+    {
+      title: "WhatsApp",
+      imageURL:
+        "https://cdn.tools.unlayer.com/social/icons/rounded/whatsapp.png",
+      link: "https://wa.me/74952015143",
+    },
+    {
+      title: "YouTube",
+      imageURL:
+        "https://cdn.tools.unlayer.com/social/icons/rounded/youtube.png",
+      link: "https://www.youtube.com/channel/UCS_eOviMlujzKWrJ2cEss8g",
+    },
+    {
+      title: "VK",
+      imageURL:
+        "https://5drops.ru/upload/medialibrary/a5e/zepoke1hn8ladb7mopnei1kvhcheqkys.png",
+      link: "https://vk.com/5drops_lab",
+    },
+  ],
+  logoImageURL:
+    "https://5drops.ru/upload/CMax/343/halyhet1wrnun5y9una2zu09zz0xbxmf.png",
+  unsubscribe: {
+    all: "{{unsubscribeUrl}}",
+    promotion: "{{Message.CancelThisSubscriptionUrl}}",
+  },
+};
+
+module.exports = OPTIONS;
